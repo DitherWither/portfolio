573bc33c6637d23bf610a670bcc11f77e1536cea
Saumya Patil <saumyasachinpatil@gmail.com>
2023-10-08 19:35:10 +0530

    Initial project skeleton

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+    return (
+        <>
+          <div className="flex justify-center items-center min-h-screen">
+            <div className="text-center ">
+                <h1 className="text-5xl font-heading">Hi There</h1>
+                <p>
+                    This site is a work in progress, and it'll have content here
+                    soon
+                </p>
+            </div>
+          </div>
+        </>
+    );
+}
+
+export default App;

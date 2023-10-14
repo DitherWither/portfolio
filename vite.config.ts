df25cc2bb00865ae3111da9e07bef3a0224e25a7
Vardhan Patil (DitherWither) <vardhanpatil@proton.me>
2023-10-14 17:29:16 +0530

    added sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import Sitemap from "vite-plugin-sitemap";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react(), Sitemap({
+      hostname: "https://vardhanpatil.com/"
+    })],
+});

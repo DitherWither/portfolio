72dced5eb44f3f9ee02ebe2b9884640f336ab005
Vardhan Patil (DitherWither) <vardhanpatil@proton.me>
2023-10-17 10:25:49 +0530

    Add crossorigin attribute to google fonts

--- app/root.tsx
+++ app/root.tsx
@@ -21,13 +21,23 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link rel="canonical" href="https://vardhanpatil.dev" />
+
+        <link
+          rel="preconnect"
+          href="https://fonts.googleapis.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter+Tight:wght@700&family=Noto+Sans&display=swap"
           rel="stylesheet"
         />
+
         <Meta />
         <Links />
       </head>
